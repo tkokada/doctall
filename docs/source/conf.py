b7b6b4c253693e6f83f5748b366522b97e000640
Takashi OKADA <tk.okada.github@gmail.com>
2022-09-19 23:17:16 +0900

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,10 +25,11 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 source_suffix = {
     '.rst': 'restructuredtext',
+    '.txt': 'markdown',
     '.md': 'markdown'
 }
